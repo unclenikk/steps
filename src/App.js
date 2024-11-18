eafc72f72ca23f7a41699d3655ddab165e0f132f
unclenikk <nikaamrazishvili@gmail.com>
2024-11-18 10:31:01 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Steps />
+
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>🙌</p>
+      </StepMessage>
+
+      <StepMessage step={1}>
+        <p>Read children prop</p>
+        <p>😶‍🌫️</p>
+      </StepMessage>
+    </div>
+  )
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  // const [test, setTest] = useState({ name: "Jonas"});
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if(step > 1) setStep((s) => s - 1);
+  }
+  
+  function handleNext() {
+    if(step < 3) setStep((s) => s + 1);
+
+    // setTest({ name: "Fred" })
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={()=> setIsOpen((is) => !is)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button 
+                bgColor="#999" 
+                textColor="#000" 
+                onClick={() => alert(`Learn how to ${messages [step - 1]}`)}
+                >
+                Learn how 
+              </Button>
+            </div>
+          </StepMessage>
+
+          <div className="buttons">
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈</span> Previous
+            </Button>
+
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next<span>👉</span>
+            </Button>
+          </div>
+        </div> 
+      )}
     </div>
   );
 }
 
-export default App;
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>
+        Step {step}
+      </h3>
+      {children}
+    </div>
+  )
+}
+
+function Button({ textColor, bgColor, onClick, children}) {
+  return (
+    <button 
+      style={{ backgroundColor: bgColor, color: textColor}}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
